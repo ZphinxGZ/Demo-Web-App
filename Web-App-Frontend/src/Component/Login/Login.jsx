5fb89adf5f1bb755710497144502d6c07e3eb0fd
chonlakornb <chonlakorn.bua@spumail.net>
2025-03-05 02:38:40 +0700

    tan & fix & bumbim eiei edit css

--- Web-App-Frontend/src/Component/Login/Login.jsx
+++ Web-App-Frontend/src/Component/Login/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import UserAndPass from '../../../public/Users';
+import './Login.css';
 
 function Login({ onLoginSuccess }) {
     const [username, setUsername] = useState('');
@@ -19,15 +20,18 @@ function Login({ onLoginSuccess }) {
     };
 
     return (
-        <div className="login-container">
-            <h2>Login</h2>
-            <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
-            <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            <button onClick={handleLogin}>Login</button>
-            {error && <p style={{ color: 'red' }}>{error}</p>}
-            <button onClick={() => navigate('/register')}>Register</button>
+        <div className="login-page">
+            <div className="login-container">
+                <h2>Login</h2>
+                <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                <button onClick={handleLogin}>Login</button>
+                {error && <p style={{ color: 'red' }}>{error}</p>}
+                <button onClick={() => navigate('/register')}>Register</button>
+            </div>
         </div>
     );
+    
 }
 
 export default Login;
