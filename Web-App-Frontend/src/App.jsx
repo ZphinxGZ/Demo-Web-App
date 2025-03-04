135f5f7e9588540639f99281ec9dd5faa5b8cde6
ZphinxGZ <zeeyou.ou@gmail.com>
2025-03-05 00:59:21 +0700

    Final

--- Web-App-Frontend/src/App.jsx
+++ Web-App-Frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import './App.css';
 import Download from './component/Download/Download';
 import Upload from './component/Upload/Upload';
-import Login from './component/Login/Login';
-import Register from './component/Register/Register';
+import Login from './Component/Login/Login';
+import Register from './Component/Register/Register';
 import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
